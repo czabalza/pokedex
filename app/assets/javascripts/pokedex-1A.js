ec38e08a05835391f962fbf723e8f85c451d142f
App Academy Student <student@appacademy.io>
2015-03-30 11:28:06 -0700

    phase 1a refresh pokemon

--- app/assets/javascripts/pokedex-1A.js
+++ app/assets/javascripts/pokedex-1A.js
@@ -1,5 +1,14 @@
 Pokedex.RootView.prototype.addPokemonToList = function (pokemon) {
+  var $li = $('<li>');
+  $li.addClass('poke-list-item');
+  $li.html(pokemon.escape('name') + ' ' + pokemon.escape('poke_type'));
+  this.$pokeList.append($li);
 };
 
 Pokedex.RootView.prototype.refreshPokemon = function () {
+  this.pokes.fetch({
+    success: function() {
+      this.pokes.each(this.addPokemonToList, this);
+    }.bind(this)
+  });
 };
